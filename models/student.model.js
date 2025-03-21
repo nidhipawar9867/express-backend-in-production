3e87028e71c91122d18f677d744cca240d75990b
Dheeraj Sapkale <sapkale.dheeraj@gmail.com>
2025-03-21 15:08:01 +0530

    add bcrypt for password hashing and custom methods added in student model

--- models/student.model.js
+++ models/student.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const studentSChema = new mongoose.Schema({
     username: String,
@@ -25,5 +26,16 @@ const studentSChema = new mongoose.Schema({
 },
     { timestamps: true }
 )
-
+studentSChema.pre("save", function (next){
+    this.password = bcrypt.hashSync(this.password, 10)
+    next()
+})
+// to create custom methods on models
+// schemaName.methods.customMethodName = function (){}
+studentSChema.methods.comparePassword = function (password){
+    return bcrypt.compareSync(password, this.password)
+}
+studentSChema.methods.fullName = function (){
+    return this.firstName + " " + this.lastName
+}
 export const Student = mongoose.model("Student", studentSChema)
